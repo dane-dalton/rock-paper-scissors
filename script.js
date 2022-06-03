00e50a934d87e4194ebff543ea91fb1e65073f6b
Dane Dalton <danedalton@sbcglobal.net>
2022-06-02 19:33:31 -0500

    starting to see problems with recursion. Will come back to finish the win counter on a fresh mind

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+let userWins = 0
 alert('Welcome! Proceed to play some good \'ole fashioned Rock, Paper, Scissors!')
 
 playSeries()
 
 function playSeries() {
-    let userWins = 0
+    userWins = 0
     let playAgain = 'y'
 
     for(let i = 0; i < 5; i++) {
-        userWins = playGame(userWins)
+        userWins = playGame()
         console.log(userWins)
     }
     if(userWins >= 3) playAgain = prompt(`You came out with ${userWins} wins in the Best of 5! Play again? (y/n)`, 'y')
@@ -17,7 +18,7 @@ function playSeries() {
 }
 
 
-function playGame(userWins) {
+function playGame() {
     let userChoice = prompt('Please choose "Rock", "Paper", or "Scissors": ', '')
     if (userChoice === '' || userChoice === null) {
         alert('Game cancelled. Proceed to play again.')
@@ -25,18 +26,28 @@ function playGame(userWins) {
     }
 
     userChoice = capitalize(userChoice)
-    userChoice = validChoice(userChoice, userWins)
+    userChoice = validChoice(userChoice)
     computerChoice = computerPlay()
     
-    userVsComputerTie(userChoice, computerChoice, userWins)
+    while(userChoice === computerChoice) {
+        alert('Game tied. Run it back!')
+        userChoice = prompt('Please choose "Rock", "Paper", or "Scissors": ', '')
+        if (userChoice === '' || userChoice === null) {
+            alert('Game cancelled. Proceed to play again.')
+            playGame()
+        }
+
+        userChoice = capitalize(userChoice)
+        userChoice = validChoice(userChoice)
+        computerChoice = computerPlay()
+    }
+
     let decision = userVsComputer(userChoice, computerChoice)
 
     userWins += decision[1]
 
     alert(decision[0])
     alert('Play again!')
-    
-    return userWins
 }
 
 
@@ -62,23 +73,16 @@ function userVsComputer(user, computer) {
     }
 }
 
-function userVsComputerTie(user, computer, userWins) {
-    if(user === computer) {
-        alert('Game tied. Run it back!')
-        playGame(userWins)
-    }
-}
-
-function validChoice(userChoice, userWins) {
-    if (userChoice === 'Rock' || userChoice === 'Paper' || userChoice === 'Scissors') return userChoice
+function validChoice(userWord) {
+    if (userWord === 'Rock' || userWord === 'Paper' || userWord === 'Scissors') return userWord
 
     alert('Invalid choice, check for spelling errors and play again.')
-    playGame(userWins)
+    playGame()
 }
 
-function capitalize(userChoice) {
-    userChoice = userChoice.charAt(0).toUpperCase() + userChoice.slice(1).toLowerCase()
-    return userChoice
+function capitalize(userWord) {
+    userWord = userWord.charAt(0).toUpperCase() + userWord.slice(1).toLowerCase()
+    return userWord
 }
 
 function computerPlay() {
