fa47fe3ae675fb948dae6b42770d44cd8f393f7e
Dane Dalton <danedalton@sbcglobal.net>
2022-06-07 12:28:30 -0500

    Started from scratch. Events only trigger on a button click. Used parseInt on the counter and that fixed the problem with the counter. Styling, a little 'flare', and making sure the win condition is correct is all thats left

--- script.js
+++ script.js
@@ -1,14 +1,117 @@
 const promptBox = document.querySelector('.prompt-content')
 const userCounter = document.querySelector('.user-score')
 const cpuCounter = document.querySelector('.cpu-score')
-
-
-
+const buttons = document.querySelectorAll('.user-btn')
+let keepGoing = true
 
 beginningPrompt()
 
-firstToFive()
-//firstToFive()
+//while (keepGoing) {
+    buttons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            let userSelection = e.target.id
+            console.log('User Selection: ' + userSelection)
+    
+            let cpuSelection = computerMove()
+            console.log('CPU Selection: ' + cpuSelection)
+    
+            if(userSelection === cpuSelection) {
+                promptBox.textContent = 'Tie!'
+            } else if (userSelection === 'Rock') {
+                if (cpuSelection === 'Paper') {
+                    promptBox.textContent = 'You lose. Paper beats rock.'
+                    cpuCounter.textContent = parseInt(cpuCounter.textContent) + 1
+                } else {
+                    promptBox.textContent = 'You win. Rock beats scissors.'
+                    userCounter.textContent = parseInt(userCounter.textContent) + 1
+                }
+            } else if (userSelection === 'Paper') {
+                if (cpuSelection === 'Scissors') {
+                    promptBox.textContent = 'You lose. Scissors beats paper.'
+                    cpuCounter.textContent = parseInt(cpuCounter.textContent) + 1
+                } else {
+                    promptBox.textContent = 'You win. Paper beats rock.'
+                    userCounter.textContent = parseInt(userCounter.textContent) + 1
+                }
+            } else {
+                if (cpuSelection === 'Rock') {
+                    promptBox.textContent = 'You lose. Rock beats scissors.'
+                    cpuCounter.textContent = parseInt(cpuCounter.textContent) + 1
+                } else {
+                    promptBox.textContent = 'You win. Scissors beats paper.'
+                    userCounter.textContent = parseInt(userCounter.textContent) + 1
+                }
+            }
+        })
+    })
+    if ( userCounter >= 5 ) {
+        promptBox.textContent = 'Congrats! You\'re the winner! Start training for nationals!'
+        keepGoing = false
+    }
+    if ( cpuCounter >= 5 ) {
+        promptBox.textContent = 'Aw rats! The computer took the win this time. Probably cheated...'
+        keepGoing = false
+    }
+//}
+
+
+// let userSelection = btnClick()
+
+// let cpuSelection = computerMove()
+
+// let keepGoing = true
+
+// while (keepGoing) {
+//     userSelection = btnClick()
+//     cpuSelection = computerMove()
+
+//     let windowClick = window.addEventListener('click', () => {
+//         if(userSelection === cpuSelection) {
+//             promptBox.textContent = 'Tie!'
+//         } else if (userSelection === 'Rock') {
+//             if (cpuSelection === 'Paper') {
+//                 promptBox.textContent = 'You lose. Paper beats rock.'
+//                 cpuCounter.textContent = parseInt(userCounter.textContent) + 1
+//             } else {
+//                 promptBox.textContent = 'You win. Rock beats scissors.'
+//                 userCounter.textContent = parseInt(userCounter.textContent) + 1
+//             }
+//         } else if (userSelection === 'Paper') {
+//             if (cpuSelection === 'Scissors') {
+//                 promptBox.textContent = 'You lose. Scissors beats paper.'
+//                 cpuCounter.textContent = parseInt(userCounter.textContent) + 1
+//             } else {
+//                 promptBox.textContent = 'You win. Paper beats rock.'
+//                 userCounter.textContent = parseInt(userCounter.textContent) + 1
+//             }
+//         } else {
+//             if (cpuSelection === 'Rock') {
+//                 promptBox.textContent = 'You lose. Rock beats scissors.'
+//                 cpuCounter.textContent = parseInt(userCounter.textContent) + 1
+//             } else {
+//                 promptBox.textContent = 'You win. Scissors beats paper.'
+//                 userCounter.textContent = parseInt(userCounter.textContent) + 1
+//             }
+//         }
+//     })
+
+
+//     console.log('userSelection: ' + userSelection)
+//     console.log('cpuSelection: ' + cpuSelection)
+//     console.log('windowClick: ' + windowClick)
+
+//     keepGoing = false
+// }
+    
+
+
+function btnClick() {
+    buttons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            return e.target.id
+        })
+    })
+}
 
 
 
@@ -28,55 +131,59 @@ function firstToFive() {
     let keepGoing = true
     let userWins = userCounter.textContent
     let cpuWins = cpuCounter.textContent
-    let userWinsInt = parseInt(userWins)
-    let cpuWinsInt = parseInt(cpuWins)
+    userCounter.textContent = '0'
+    cpuCounter.textContent = '0'
 
-    userWins = '0'
-    cpuWins = '0'
+    console.log('userWins: ' + userWins)
+    console.log('cpuWins: ' + cpuWins)
 
     while(keepGoing) {
 
-        [userWinsInt, cpuWinsInt] = playGame(userWins, cpuWins)
+        updateCounter = playGame()
+
+        console.log('updateCounter: ' + updateCounter)
         
-        userCounter.textContent = userWinsInt
-        cpuCounter.textContent = cpuWinsInt
+        userWins = parseInt(userCounter.textContent) + updateCounter[0]
+        cpuWins = parseInt(cpuCounter.textContent) + updateCounter[1]
+
+        userCounter.textContent = userWins
+        cpuCounter.textContent = cpuWins
         
 
-        if (userWinsInt >= 5) {
+        if (userWins >= 5) {
             keepGoing = false
             promptBox.textContent = "Congrats! You achieved 5 wins before that pesky computer!"
         }
-        if (cpuWinsInt >= 5) {
+        if (cpuWins >= 5) {
             keepGoing = false
             promptBox.textContent = "Uh oh! Looks like you lost to the computer. It was probably cheating anyway..."
         }
+        keepGoing = false
     }
 }
 
 
-function playGame(userW, cpuW) {
+function playGame() {
 
     //transform win counter from string to int to avoid concat
-    let userInt = parseInt(userW)
-    let cpuInt = parseInt(cpuW)
-    let gameDecision
 
-    [userInt, cpuInt] = userMove()
+  // while(gameDecision === null || gameDecision === undefined) {
 
-    //computerChoice = computerMove()
+        let gameDecision = userMove()
+        
+        console.log('gameDecision: ' + gameDecision)
+    //}
     
-    //get new choices when a tie occurs
-    // while(userChoice === computerChoice) {
-    //     promptBox.textContent = 'Tie! Pick again.'
+    if (gameDecision === null || gameDecision === undefined) {
+        gameDecision = userMove()
+        console.log('gameDecision2: ' + gameDecision)
+    } else {
+        console.log('gameDecision Return: ' + gameDecision)
+        return gameDecision
+    }
 
-    //     userChoice = userMove()
 
-    //     //computerChoice = computerMove()
-    // }
-
-    //let decision = userVsComputer(userChoice, computerChoice)
-
-    return [userInt, cpuInt]
+    
 }
 
 
@@ -130,6 +237,8 @@ function userMove() {
                 computerSelection = computerMove()
             }
             let winner = userVsComputer(userSelection, computerSelection)
+
+            console.log('winner: ' + winner)
 
             return winner
         })
