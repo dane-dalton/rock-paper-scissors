310137ccb4a767d8c21ae071044e57ca91730079
Dane Dalton <danezdalton1@gmail.com>
2023-05-29 17:01:45 -0500

    script changed from var to const and still works

--- load-mathjax.js
+++ load-mathjax.js
@@ -9,9 +9,8 @@ window.MathJax = {
     fontCache: "global",
   },
 }
-
 ;(function () {
-  var script = document.createElement("script")
+  const script = document.createElement("script")
   script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js"
   script.async = true
   document.head.appendChild(script)
